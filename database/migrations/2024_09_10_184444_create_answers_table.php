a3c4eb495aed49eed11e68e11708266bfc86fefd
ychihiro <y.chihiro0510@gmail.com>
2024-09-10 19:03:42 +0900

    feat: テーブル作成

---
+++ database/migrations/2024_09_10_184444_create_answers_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname', 200);
+            $table->tinyInteger('gender')->comment('1: 男性, 2: 女性');
+            $table->foreignId('age_id')->constrained();
+            $table->string('email');
+            $table->tinyInteger('is_send_email')->comment('0: 送信許可, 1: 送信不可');
+            $table->text('feedback');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('answers');
+    }
+};
