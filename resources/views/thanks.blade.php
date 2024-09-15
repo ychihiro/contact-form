6eb16acc61d49fbcf7b6660e1617388193d5c9df
ychihiro <y.chihiro0510@gmail.com>
2024-09-15 19:22:51 +0900

    feat: サンクス画面作成

---
+++ resources/views/thanks.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ContactForm</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/thanks.css">
+</head>
+<body>
+    <div class="container">
+        <div class="thanks-container">
+            <p class="title">ご意見をお送りいただきありがとうございました</p>
+            <a href={{route('feedback.index')}} class="button-main">トップページへ戻る</a>
+        </div>
+    </div>
+</body>
+</html>
